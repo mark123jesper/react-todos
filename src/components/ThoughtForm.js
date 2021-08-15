52a751fe4df7112b0f5c5a3343922f96738fbca7
Mark Jesper Pillerva <mark123jesper@yahoo.com>
2021-08-15 10:15:24 +0800

    Fixed Manifest and Placeholder

--- src/components/ThoughtForm.js
+++ src/components/ThoughtForm.js
@@ -79,7 +79,7 @@ const ThoughtForm = (props, { deleteAllHandler }) => {
                             <textarea
                                 className='form-control  mt-1'
                                 value={input}
-                                placeholder='Place To-Do'
+                                placeholder='Place your Thoughts Here'
                                 onChange={handleChange}
                                 ref={inputRef}
                                 rows='10'
